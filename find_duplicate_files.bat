f4d67bd897e369fcf257529caf59cdca4f70e5c8
calebTree <ande4905@gmail.com>
2021-06-09 15:08:54 -0500

    looking good

--- find_duplicate_files.bat
+++ find_duplicate_files.bat
@@ -1,3 +1,11 @@
-@echo off 
+@echo off
+SETLOCAL EnableDelayedExpansion
+SET "x = 0" 
 
-for /f tokens^=* %%i in ('where /R %cd% *.*')do certutil -hashfile "%%~i" SHA1
+FOR /F tokens^=* %%i IN ('where /R %cd% *.*') DO (
+	CALL ECHO path = "%%i"
+	FOR /F tokens^=* %%G IN ('certutil -hashfile "%%i" SHA1 ^| findstr /V ":"') DO SET _hash=%%G
+	SET obj[%x%].hash=!_hash!
+	CALL ECHO hash = %%obj[%x%].hash%%
+	SET /a "x+=1"
+)
