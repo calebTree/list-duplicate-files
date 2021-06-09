d95b69ddd28c9f2debdd1438d7dcff1c20af1d67
calebTree <ande4905@gmail.com>
2021-06-09 13:47:07 -0500

    gets all hashes

--- find_duplicate_files.bat
+++ find_duplicate_files.bat
@@ -1,12 +1,3 @@
 @echo off 
 
-set obj[0].Name=Joe 
-set obj[0].ID=1 
-set obj[1].Name=Mark 
-set obj[1].ID=2 
-set obj[2].Name=Mohan 
-set obj[2].ID=3 
-FOR /L %%i IN (0 1 2) DO  (
-   call echo Name = %%obj[%%i].Name%%
-   call echo Value = %%obj[%%i].ID%%
-)
+for /f tokens^=* %%i in ('where /R %cd% *.*')do certutil -hashfile "%%~i" SHA1
