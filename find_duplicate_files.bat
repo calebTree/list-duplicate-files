0bac8ea3d4ab93286aa3e70cfdd87f14816548bb
calebTree <ande4905@gmail.com>
2021-06-10 12:19:26 -0500

    generates 3 txt files; last of which is reduced to unique duplicate hashes; reports if no matches are located

--- find_duplicate_files.bat
+++ find_duplicate_files.bat
@@ -1,7 +1,7 @@
-@echo off
+@ECHO OFF
 SETLOCAL EnableDelayedExpansion
 
-:: loop directory and set hash and path object
+:: loop directory and set hash/path object
 SET /A x=0
 FOR /F "tokens=*" %%i IN ('where /R %cd% *.*') DO (
 	SET obj[!x!].path=%%i
@@ -14,7 +14,7 @@ FOR /F "tokens=*" %%i IN ('where /R %cd% *.*') DO (
 	SET /a x+=1
 )
 
-:: get length of object of files
+:: get length of complete object of files
 set /a A=0
 :SymLoop
 if defined obj[%A%].hash (
@@ -30,34 +30,57 @@ FOR /L %%i IN (0 1 %A%) DO (
 )
 echo.
 
-:: output hashes to txt
+:: output all hashes to txt
 IF NOT EXIST hashes.txt (
 	FOR /L %%p IN (0 1 %A%) DO (
 		ECHO !obj[%%p].hash! >> hashes.txt
 	)
 )
 
-SET prev=
-SET _done=false
-FOR /F "tokens=* delims=" %%a IN ('sort hashes.txt') DO CALL :checkduplicate %%a
-
-:checkduplicate
-IF "%1"=="%prev%" (
-	ECHO DONE: !_done!
-	IF "!_done!"=="false" (
-		ECHO Duplicate file SHA1: %1
-		ECHO Paths:
-		FOR /L %%r IN (0 1 %A%) DO (
-			IF "!obj[%%r].hash!"=="%1" ECHO !obj[%%r].path! 
+:: output all duplicate hashes to txt
+IF NOT EXIST duplicates.txt (
+	SET prev=
+	FOR /F "tokens=* delims=" %%a IN ('sort hashes.txt') DO (
+		IF "%%a"=="!prev!" (
+			ECHO %%a >> duplicates.txt
 		)
-		ECHO.
-		SET _done=true
+		SET prev=%%a
+	)
+	IF NOT EXIST duplicates.txt (
+		ECHO No duplicates^^!
+		GOTO :EOF
 	)
 )
-SET prev=%1
+
+:: output unique hashes to txt
+IF NOT EXIST unq_duplicates.txt (
+	FOR /F "tokens=* delims=" %%a IN ('sort duplicates.txt') DO (
+		IF NOT "%%a"=="!prev!" (
+			ECHO %%a >> unq_duplicates.txt
+		)
+		SET prev=%%a
+	)
+)
+
+:: output all paths and shas of duplicate files to console
+IF EXIST unq_duplicates.txt (
+	FOR /F "tokens=* delims=" %%a IN ('sort unq_duplicates.txt') DO CALL :checkduplicate %%a
+
+	:checkduplicate
+	IF "%1" NEQ "" (
+		ECHO Duplicate file SHA1: "%1"
+		ECHO Paths:
+		FOR /L %%r IN (0 1 %A%) DO (
+			REM ECHO r: %%r
+			IF "!obj[%%r].hash!"=="%1" ECHO !obj[%%r].path!
+		)
+		SET prev=%1		
+		ECHO.
+	)
+)
 
 :Trim
 SetLocal EnableDelayedExpansion
 set Params=%*
-for /f "tokens=1*" %%a in ("!Params!") do EndLocal & set %1=%%b
+for /f "tokens=1*" %%a IN ("!Params!") DO EndLocal & SET %1=%%b
 exit /b
