fe13afd44576715ec95b9d568ac34b821c3a9c92
calebTree <ande4905@gmail.com>
2021-06-11 04:45:47 -0500

    beautified

--- find_duplicate_files.bat
+++ find_duplicate_files.bat
@@ -1,86 +1,101 @@
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
-
+ECHO.
+ECHO Searching: %cd%\ for duplicate files ...
+ECHO.
 :: loop directory and set hash/path object
-SET /A x=0
+CALL :ProgressMeter 0
+SET /A "_size=0"
 FOR /F "tokens=*" %%i IN ('where /R %cd% *.*') DO (
-	SET obj[!x!].path=%%i
+	SET "obj[!_size!].path=%%i"
 	FOR /F "tokens=*" %%G IN ('certutil -hashfile "%%i" SHA1 ^| findstr /V ":"') DO (
-		REM CALL :Trim %%G
-		REM ECHO G "%%G"
-		SET _hash=%%G
+		SET "_hash=%%G"
 	)
-	SET obj[!x!].hash=!_hash!
-	SET /a x+=1
+	SET "obj[!_size!].hash=!_hash!"
+	SET /A "_size+=1"
 )
+SET /A "_size-=1"
+REM ECHO.
+REM SET obj
 
-:: get length of complete object of files
-set /a A=0
-:SymLoop
-if defined obj[%A%].hash (
-   set /a "A+=1"
-   GOTO :SymLoop 
-)
-SET /a A-=1
-
-:: output all paths and hashes to console
-FOR /L %%i IN (0 1 %A%) DO (
-   ECHO path = !obj[%%i].path!
-   ECHO hash = "!obj[%%i].hash!"
-)
-echo.
-
+CALL :ProgressMeter 20
 :: output all hashes to txt
 IF NOT EXIST hashes.txt (
-	FOR /L %%p IN (0 1 %A%) DO (
+	FOR /L %%p IN (0 1 %_size%) DO (
 		ECHO !obj[%%p].hash! >> hashes.txt
 	)
 )
 
+CALL :ProgressMeter 40
 :: output all duplicate hashes to txt
 IF NOT EXIST duplicates.txt (
-	SET prev=
+	SET "prev="
 	FOR /F "tokens=* delims=" %%a IN ('sort hashes.txt') DO (
 		IF "%%a"=="!prev!" (
 			ECHO %%a >> duplicates.txt
 		)
-		SET prev=%%a
+		SET "prev=%%a"
 	)
+	DEL hashes.txt
 	IF NOT EXIST duplicates.txt (
 		ECHO No duplicates^^!
 		GOTO :EOF
 	)
 )
 
-:: output unique hashes to txt
+CALL :ProgressMeter 60
+:: output only unique hashes to txt
 IF NOT EXIST unq_duplicates.txt (
 	FOR /F "tokens=* delims=" %%a IN ('sort duplicates.txt') DO (
 		IF NOT "%%a"=="!prev!" (
 			ECHO %%a >> unq_duplicates.txt
+			SET /A "_count+=1"
 		)
-		SET prev=%%a
+		SET "prev=%%a"
 	)
+	DEL duplicates.txt
+)
+(SET LF=^
+%=this line is empty=%
 )
 
+ECHO %_count% duplicates found.
+ECHO %_count% duplicates found.!LF! > %userprofile%\Desktop\%_count%_duplicates.txt
+ECHO The SHA1 and file paths are in "%userprofile%\Desktop\%_count%_duplicates.txt".
+
+CALL :ProgressMeter 80
 :: output all paths and shas of duplicate files to console
 IF EXIST unq_duplicates.txt (
-	FOR /F "tokens=* delims=" %%a IN ('sort unq_duplicates.txt') DO CALL :checkduplicate %%a
-
-	:checkduplicate
-	IF "%1" NEQ "" (
-		ECHO Duplicate file SHA1: "%1"
-		ECHO Paths:
-		FOR /L %%r IN (0 1 %A%) DO (
-			REM ECHO r: %%r
-			IF "!obj[%%r].hash!"=="%1" ECHO !obj[%%r].path!
-		)
-		SET prev=%1		
-		ECHO.
+	FOR /F "tokens=*" %%a IN (unq_duplicates.txt) DO (
+		CALL :checkduplicate %%a >> %userprofile%\Desktop\%_count%_duplicates.txt
 	)
+	DEL unq_duplicates.txt
 )
 
-:Trim
-SetLocal EnableDelayedExpansion
-set Params=%*
-for /f "tokens=1*" %%a IN ("!Params!") DO EndLocal & SET %1=%%b
-exit /b
+:checkduplicate
+IF "%1" NEQ "" (
+	ECHO Duplicate file SHA1: %1
+	ECHO Paths:
+	FOR /L %%r IN (0 1 %_size%) DO (
+		IF "!obj[%%r].hash!"=="%1" ECHO !obj[%%r].path!
+	)
+	ECHO.
+)
+
+CALL :ProgressMeter 100
+EXIT /B
+
+:ProgressMeter
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET ProgressPercent=%1
+SET /A NumBars=%ProgressPercent%/2
+SET /A NumSpaces=50-%NumBars%
+
+SET Meter=
+
+FOR /L %%A IN (%NumBars%,-1,1) DO SET Meter=!Meter!I
+FOR /L %%A IN (%NumSpaces%,-1,1) DO SET Meter=!Meter!
+
+TITLE Overall Progress:  [%Meter%]  %ProgressPercent%%%
+ENDLOCAL
+GOTO :EOF
