ab3462b2415a932596998e86b9d131f4ef3a14c5
calebTree <ande4905@gmail.com>
2021-06-11 19:55:22 -0500

    much improved colors and interface

--- find_duplicate_files.bat
+++ find_duplicate_files.bat
@@ -1,26 +1,39 @@
 @if (@CodeSection == @Batch) @then
 @ECHO OFF
 SETLOCAL EnableDelayedExpansion
-ECHO.
-:: loop directory and set hash/path object
-ECHO [36mSearching: "%cd%" for duplicate files.[0m
-CALL :ProgressMeter 10
-SET /A "_size=0"
+ECHO Counting files ...
+CALL :ProgressMeter 0
+SET /A "_sizeA=0"
 FOR /F "tokens=*" %%i IN ('where /R "%cd%" *.*') DO (
-	CALL :drawProgressBar /
+	SET /A "_sizeA+=1"
+)
+ECHO                                    [41m---WARNING---[0m
+ECHO [36mReady to SHA1 hash [47m[30m%_sizeA%[0m[36m files? (y/n)[0m
+SET /P "_input=[36mEnter Yes or No:[0m "
+IF /I "%_input%"=="y" GOTO :yes
+GOTO :EOF
+
+:yes
+:: loop directory and set hash/path object
+SET /A "_size=0"
+CALL :ProgressMeter 10
+FOR /F "tokens=*" %%i IN ('where /R "%cd%" *.*') DO (
+	CALL :drawProgressBar / !_sizeA!
 	SET "obj[!_size!].path=%%i"
 	FOR /F "tokens=*" %%j IN ('certutil -hashfile "%%i" SHA1 ^| findstr /V ":"') DO (
 		SET "_hash=%%j"
-		CALL :drawProgressBar -
+		CALL :drawProgressBar - !_sizeA!
 	)
 	SET "obj[!_size!].hash=!_hash!"
 	SET /A "_size+=1"
-	CALL :drawProgressBar \
+	CALL :drawProgressBar \ !_sizeA!
+	SET /A "_sizeA-=1"
 )
 SET /A "_size-=1"
 REM ECHO.
 REM SET obj
-
+ECHO.
+ECHO [36mSearching for duplicates ...[0m
 CALL :ProgressMeter 20
 :: output all hashes to txt
 IF NOT EXIST hashes.txt (
@@ -61,7 +74,6 @@ IF NOT EXIST unq_duplicates.txt (
 (SET LF=^
 
 )
-ECHO.
 ECHO.
 ECHO [31m%_count% duplicates found.[0m
 ECHO.
@@ -108,7 +120,7 @@ GOTO :EOF
 
 :drawProgressBar
 for /f %%a in ('copy "%~f0" nul /z') do set "pb.cr=%%a"
-<nul set /p "=[33mPlease Wait [ %1 ] ... !pb.cr![0m"
+<nul set /p "=[32mComputing [ %2 ] SHA1 file hashes in: "%cd%". Please Wait [ %1 ] ...!pb.cr![0m"
 call :pause 0
 GOTO :EOF
 
